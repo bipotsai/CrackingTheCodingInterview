4702b4f808cead4f25236b44341a51b099cb7b58
bipo <bipolet@gmail.com>
2017-09-13 13:28:58 +0100

    chapter 1 question 1.7 Rotate Matrix

--- src/ctci/chapter1/arraysandstrings/RotateMatrix.java
+++ src/ctci/chapter1/arraysandstrings/RotateMatrix.java
@@ -2,6 +2,10 @@ package ctci.chapter1.arraysandstrings;
 
 public class RotateMatrix {
 
+    /**
+     * self implemented
+     * @param input
+     */
     public void rotate90(int[][] input){
         int N = input.length;
         int i=0;
@@ -43,6 +47,37 @@ public class RotateMatrix {
         }
     }
 
+    /**
+     * solution from book
+     * @param matrix
+     */
+    public static boolean rotate(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length) return false; // Not a square
+        int n = matrix.length;
+
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for(int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i]; // save top
+
+                // left -> top
+                matrix[first][i] = matrix[last-offset][first];
+
+                // bottom -> left
+                matrix[last-offset][first] = matrix[last][last - offset];
+
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                // top -> right
+                matrix[i][last] = top; // right <- saved top
+            }
+        }
+        return true;
+    }
+
     public void print(int[][] input){
         for(int[] row : input){
             for(int n : row){
@@ -59,6 +94,11 @@ public class RotateMatrix {
         System.out.println("");
         rm.print(input);
         rm.rotate90(input);
+        System.out.println("");
+        rm.print(input);
+
+        input = new int[][]{{1,2,3,4,5},{16,17,18,19,6},{15,24,25,20,7},{14,23,24,21,8},{13,12,11,10,9}};
+        RotateMatrix.rotate(input);
         System.out.println("");
         rm.print(input);
     }
