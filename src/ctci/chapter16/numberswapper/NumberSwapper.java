cad568ebbbef39ed1f002d70d667edc17ffe0df7
bipo <bipolet@gmail.com>
2018-03-12 21:48:06 +0000

    chapter 16 - NumberSwapper 2 solutions

---
+++ src/ctci/chapter16/numberswapper/NumberSwapper.java
@@ -0,0 +1,37 @@
+package ctci.chapter16.numberswapper;
+
+public class NumberSwapper {
+
+    public static void swap(int x, int y) {
+        System.out.printf("%d %d ----> ", x, y);
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+        System.out.printf("%d %d \n", x, y);
+    }
+
+    public static void swap2(int x, int y) {
+        System.out.printf("%d %d ----> ", x, y);
+        x = x - y;
+        y = x + y;
+        x = y - x;
+        System.out.printf("%d %d \n", x, y);
+    }
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+        NumberSwapper.swap(x, y);
+        NumberSwapper.swap2(x, y);
+
+
+        int a = 99999999;
+        int b = 77777777;
+        NumberSwapper.swap(a, b);
+        NumberSwapper.swap2(a, b);
+
+    }
+
+
+}
