278eb8b341fb1b11dc33b8097c6c37b664c06437
bipo <bipolet@gmail.com>
2018-03-12 22:00:30 +0000

    chapter 16 - WordFrequencies stream and Lambda

--- src/ctci/chapter16/wordfrequencies/WordFrequencies.java
+++ src/ctci/chapter16/wordfrequencies/WordFrequencies.java
@@ -1,4 +1,19 @@
 package ctci.chapter16.wordfrequencies;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class WordFrequencies {
+
+    public static int getFrequencies(String[] book, String word) {
+        HashMap<String, Integer> countMap = new HashMap<>();
+        Arrays.stream(book).forEach(s -> countMap.put(s, countMap.getOrDefault(s, 0) + 1));
+        return countMap.getOrDefault(word, 0);
+    }
+
+
+    public static void main(String[] args) {
+        String[] book = {"the", "Lara", "and", "outcropping", "career", "it"};
+        System.out.printf("%s : %s \n", "the", WordFrequencies.getFrequencies(book,"the"));
+    }
 }
